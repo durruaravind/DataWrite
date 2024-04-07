a24bc5d454ea09871b0ee7851d744c1f9c453abf
Durru Aravind <aravind2000d@gmail.com>
2024-04-07 23:32:29 +0530

    little formatting

--- DataWrite.c
+++ DataWrite.c
@@ -9,21 +9,21 @@ int Day()
     time_t t;
     t = time(NULL);
     struct tm tm = *localtime(&t);
-    return(tm.tm_mday);
+    return (tm.tm_mday);
 }
 int Month()
 {
     time_t t;
     t = time(NULL);
     struct tm tm = *localtime(&t);
-    return((tm.tm_mon+1));
+    return ((tm.tm_mon + 1));
 }
 int Year()
 {
     time_t t;
     t = time(NULL);
     struct tm tm = *localtime(&t);
-    return((tm.tm_year+1900));
+    return ((tm.tm_year + 1900));
 }
 int Hour()
 {
@@ -51,33 +51,32 @@ int TimeCheck()
     time_t t;
     t = time(NULL);
     struct tm tm = *localtime(&t);
-    if(tm.tm_hour==23 && tm.tm_min>=59 && tm.tm_sec>=59)
-        return(1);
+    if (tm.tm_hour == 23 && tm.tm_min >= 59 && tm.tm_sec >= 59)
+        return (1);
     else
-        return(0);
+        return (0);
 }
 
 int main()
 {
-    char filepath[100]="";
-    char path[]= "Data_";
+    char filepath[100] = "";
+    char path[] = "Data_";
     // if you want to save to specific path then use this code: char path[]= "E://Data_";
     sprintf(filepath, "%s%d-%d-%d.csv", path, Day(), Month(), Year());
 
-    fptr= fopen(filepath, "a");
+    fptr = fopen(filepath, "a");
 
-    float var[]= {11,22,33,44,55,66,77,88,99,0};
-    if(TimeCheck()!=0)
+    float var[] = {11, 22, 33, 44, 55, 66, 77, 88, 99, 0};
+    if (TimeCheck() != 0)
     {
         fclose(fptr);
         exit(1);
     }
     else
     {
-        fprintf(fptr, "%d-%d-%d,%d:%d:%d,,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n", 
-        Day(), Month(), Year(), Hour(), Minutes(), Seconds(),  
-        var[0], var[1], var[2], var[3], var[4], var[5], var[6], var[7], var[8], var[9]);
-
+        fprintf(fptr, "%d-%d-%d,%d:%d:%d,,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+                Day(), Month(), Year(), Hour(), Minutes(), Seconds(),
+                var[0], var[1], var[2], var[3], var[4], var[5], var[6], var[7], var[8], var[9]);
     }
     return 0;
 }
